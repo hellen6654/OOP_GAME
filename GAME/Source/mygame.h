98fb09702b54a5bcfa66277cc1abfeae5b9b5add
t105590028 <t105590028@ntut.org.tw>
2018-05-25 00:27:38 +0800

    車廂剩一個了

--- GAME/Source/mygame.h
+++ GAME/Source/mygame.h
@@ -54,17 +54,17 @@ namespace game_framework
 // Constants
 /////////////////////////////////////////////////////////////////////////////
 
-	enum AUDIO_ID  				// 定義各種音效的編號
-	{
-		AUDIO_SLIP,				// 0
-		AUDIO_CLICK,			// 1
-		AUDIO_MUSIC,			// 2
-		AUDIO_SELECT1,			// 3
-		AUDIO_SELECT2,			// 4
-		AUDIO_APPEAR,			// 5
-		AUDIO_STOP1,			// 6
-		AUDIO_STOP2				// 7
-	};
+enum AUDIO_ID  				// 定義各種音效的編號
+{
+    AUDIO_SLIP,				// 0
+    AUDIO_CLICK,			// 1
+    AUDIO_MUSIC,			// 2
+    AUDIO_SELECT1,			// 3
+    AUDIO_SELECT2,			// 4
+    AUDIO_APPEAR,			// 5
+    AUDIO_STOP1,			// 6
+    AUDIO_STOP2				// 7
+};
 
 /////////////////////////////////////////////////////////////////////////////
 // 這個class為遊戲的遊戲開頭畫面物件
@@ -84,15 +84,15 @@ class CGameStateInit : public CGameState
     protected:
         void OnShow();									// 顯示這個狀態的遊戲畫面
     private:
-		CMovingBitmap start;							//遊戲開始的按鈕圖
-		CMovingBitmap startInvert;						//遊戲開始的顏色反相按鈕圖
+        CMovingBitmap start;							//遊戲開始的按鈕圖
+        CMovingBitmap startInvert;						//遊戲開始的顏色反相按鈕圖
         CMovingBitmap end;								//遊戲結束的按鈕圖
-		CMovingBitmap endInvert;						//遊戲結束的顏色反相按鈕圖
+        CMovingBitmap endInvert;						//遊戲結束的顏色反相按鈕圖
         CMovingBitmap logo;								//遊戲的logo
-		bool isMouseInStartBtn;							//滑鼠在開始按鈕
-		bool isMouseInEndBtn;							//滑鼠在結束按鈕			
+        bool isMouseInStartBtn;							//滑鼠在開始按鈕
+        bool isMouseInEndBtn;							//滑鼠在結束按鈕
         int mouse_x = 0, mouse_y = 0;					//滑鼠位置
-		
+        int mouse_state = 1;
 };
 
 /////////////////////////////////////////////////////////////////////////////
@@ -119,46 +119,60 @@ class CGameStateRun : public CGameState
         void OnShow();									// 顯示這個狀態的遊戲畫面
     private:
         CMovingBitmap  map;
-		CMovingBitmap  color[7];
-		const int STATION_APPERAED_TIME = 5;			//車站出現的秒數
-		const int PASSENAGER_APPERAED_TIME = 3;			//乘客出現的秒數
+        CMovingBitmap  color[7];
+        const int STATION_APPERAED_TIME = 5;			//車站出現的秒數
+        const int PASSENAGER_APPERAED_TIME = 3;			//乘客出現的秒數
         const int MAXIUM_STATION = 10;					//會出現在地圖上車站總數
         const int MAXIUM_STATION_TYPE = 7;				//車站樣式總共有7個 00~06
-		const int LINE_COLOR_NUM = 7;					//車站樣式總共有7個 00~06
+        const int LINE_COLOR_NUM = 7;					//車站樣式總共有7個 00~06
 
-		const int MAXIUM_PASSANGER = MAXIUM_STATION * 5; //乘客總個數
+        const int MAXIUM_PASSANGER = MAXIUM_STATION * 5; //乘客總個數
 
-		//station.h裡面也有相同的屬性要改
-        const int MAX_GAME_MAP_SIDE_X = 770;			//實際上的遊戲邊界X軸只有到770 
+        //station.h裡面也有相同的屬性要改
+        const int MAX_GAME_MAP_SIDE_X = 770;			//實際上的遊戲邊界X軸只有到770
         const int MIN_GAME_MAP_SIDE_X = 30;			    //實際上的遊戲邊界X軸從30開始
         const int MAX_GAME_MAP_SIDE_Y = 560;			//實際上的遊戲邊界Y軸只有到560
         const int MIN_GAME_MAP_SIDE_Y = 60;			    //實際上的遊戲邊界Y軸從60開始
-		
-		bool isStop;
-		
+
+        bool isStop;
+
         int currentStationNum;							//目前出現到哪個車站
-		int currentPassenagerNum;						//目前有幾個乘客出現
+        int currentPassenagerNum;						//目前有幾個乘客出現
         int counter;
         int clickedX;
         int clickedY;
 
-        Line    *line;									//負責處理火車線路的指標
+        Line*    line;									//負責處理火車線路的指標
 
-		Line    redLine;								//紅色線路 
-		Line    orangeLine;								//橘色線路 
-		Line    yellowLine;								//黃色線路
-		Line    greenLine;								//綠色線路
-		Line    blueLine;								//藍色線路
-		Line    bblueLine;								//靛色線路
-		Line    purpleLine;								//紫色線路
-		
+        Line    redLine;								//紅色線路
+        Line    orangeLine;								//橘色線路
+        Line    yellowLine;								//黃色線路
+        Line    greenLine;								//綠色線路
+        Line    blueLine;								//藍色線路
+        Line    bblueLine;								//靛色線路
+        Line    purpleLine;								//紫色線路
+
         Clock   clock;									//會動的時鐘
         Week    week;									//會動的周次
 
         vector<Station> stationList;					//一堆的車站
-		vector<Passenger> passengerList;				//一堆的乘客
-		vector<Cabin> cabinList;
-		
+        vector<Passenger> passengerList;				//一堆的乘客
+        vector<Cabin> redCabinList;
+        vector<Cabin> orangCabinList;
+        vector<Cabin> yellowCabinList;
+        vector<Cabin> greenCabinList;
+        vector<Cabin> blueCabinList;
+        vector<Cabin> bblueCabinList;
+        vector<Cabin> pupleCabinList;
+
+        int redCabinState = 0;
+        int orangCabinState = 0;
+        int yellowCabinState = 0;
+        int greenCabinState = 0;
+        int blueCabinState = 0;
+        int bblueCabinState = 0;
+        int pupleCabinState = 0;
+
         int mouse_x = 0, mouse_y = 0;					//滑鼠位置
 };
 
